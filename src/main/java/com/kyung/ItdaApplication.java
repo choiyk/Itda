f8483ccf19c0ca81bcfec841cabcd8324383f017
choiyk <d_pvmf@naver.com>
2018-05-21 14:55:39 +0900

    first commit

---
+++ src/main/java/com/kyung/ItdaApplication.java
@@ -0,0 +1,12 @@
+package com.kyung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItdaApplication.class, args);
+	}
+}
