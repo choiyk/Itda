716015fdd8d1125874cf0ffd2a422ab05bb0b12e
choiyk <d_pvmf@naver.com>
2018-06-11 15:50:56 +0900

    controller에서 alert창 띄우기

--- src/main/java/com/kyung/controller/UserController.java
+++ src/main/java/com/kyung/controller/UserController.java
@@ -12,34 +12,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kyung.dto.Department;
 import com.kyung.dto.User;
 import com.kyung.model.UserModificationModel;
 import com.kyung.model.UserPasswdModificationModel;
 import com.kyung.model.UserPasswordCheckModel;
 import com.kyung.model.UserRegistrationModel;
-import com.kyung.dto.Department;
+import com.kyung.service.DepartmentService;
 import com.kyung.service.UserService;
 import com.kyung.utils.Encryption;
-import com.kyung.service.DepartmentService;
 
 @Controller
 public class UserController {
 	@Autowired UserService userService;
 	@Autowired DepartmentService departmentService;
-	
+
 	@RequestMapping("main")
 	public String main()
 	{
 		System.out.println("login success");
 		return "user/main";
 	}
-	
+
 	@RequestMapping("mypage")
 	public String mypage()
 	{
 		return "user/mypage";
 	}
-	
+
 	@RequestMapping(value="myinfo_setting", method=RequestMethod.GET)
 	public String myinfoSetting(UserModificationModel userModel, Model model)
 	{
@@ -51,30 +51,35 @@ public class UserController {
 		model.addAttribute("departments", departmentService.findAll());
 		return "user/myinfo_setting";
 	}
-	
+
 	@RequestMapping(value="myinfo_setting", method=RequestMethod.POST)
 	public String myinfoSetting(@Valid UserModificationModel userModel, BindingResult bindingResult, Model model)
 	{
 		System.out.println("정보 변경");
 		User currentUser = userService.getCurrentUser();
 		UserRegistrationModel userRegModel = userModel.toRegistrationUser(currentUser);
-		
-		if(bindingResult.hasErrors())
+
+		/*if(bindingResult.hasErrors())
 		{
 			System.out.println("정보 변경 에러");
 			User user = userService.getCurrentUser();
 			model.addAttribute("user", user);
 			model.addAttribute("departments", departmentService.findAll());
 			return "user/myinfo_setting";
-		}
+		}*/
 		System.out.println("정보변경 진행중");
 		User user = userRegModel.toUser();
 		System.out.println("정보변경 진행중2");
 		userService.edit(user);
 		System.out.println("정보변경 성공");
-		return "redirect:mypage";
+
+		model.addAttribute("msg", "정보가 변경되었습니다.");
+		model.addAttribute("url", "mypage");
+		return "redirect";
+
+		/*return "redirect:mypage";*/
 	}
-	
+
 
 	@RequestMapping(value="mypw_auth", method=RequestMethod.GET)
 	public String mypw_auth(UserPasswordCheckModel userModel, Model model)
@@ -91,9 +96,9 @@ public class UserController {
 		user = userService.findOne(user.getId());
 		String password = userModel.getPassword();
 		password = Encryption.encrypt(password, Encryption.SHA256);
-		
+
 		if(userService.passwordComparison(user.getPassword(), password, bindingResult)) // equal == true
-		{	
+		{
 			System.out.println("pw 일치");
 			return "redirect:mypw_setting";
 		}
@@ -103,7 +108,7 @@ public class UserController {
 			return "user/mypw_auth";
 		}
 	}
-	
+
 	@RequestMapping(value="mypw_setting", method=RequestMethod.GET)
 	public String mypwSetting(UserPasswdModificationModel userModel, Model model)
 	//public String mypwSetting()
@@ -111,17 +116,17 @@ public class UserController {
 		System.out.println("pw setting get");
 		return "user/mypw_setting";
 	}
-	
+
 	@RequestMapping(value="mypw_setting", method=RequestMethod.POST)
 	public String mypwSetting(@Valid UserPasswdModificationModel userModel, BindingResult bindingResult, Model model)
 	{
 		System.out.println("pw setting post");
-		if(bindingResult.hasErrors()) 
+		if(bindingResult.hasErrors())
 		{
 			System.out.println("pw1,2 형식 에러");
 			return "user/mypw_setting";
 		}
-		if(userService.passwordComparisonEdit(userModel.getPassword1(), userModel.getPassword2(), bindingResult)) 
+		if(userService.passwordComparisonEdit(userModel.getPassword1(), userModel.getPassword2(), bindingResult))
 		{
 			System.out.println("pw1,2 같음");
 			User user = userService.getCurrentUser();
@@ -149,18 +154,18 @@ public class UserController {
 	{
 		return "user/my_comment";
 	}
-	
+
 /*
 	@RequestMapping("userList")
-	public String allUserList(Model model) 
+	public String allUserList(Model model)
 	{
 		List<User> users = userService.findAll();
 		model.addAttribute("users",users);
 		return "admin/userList";
 	}
-*/	
+*/
 	@RequestMapping(value="create", method=RequestMethod.GET)
-	public String create(Model model) 
+	public String create(Model model)
 	{
 		User user = new User();
 		List<Department> departments = departmentService.findAll();
@@ -168,16 +173,16 @@ public class UserController {
 		model.addAttribute("departments", departments);
 		return "user/edit";
 	}
-	
+
 	@RequestMapping(value="create", method=RequestMethod.POST)
-	public String create(Model model, User user) 
+	public String create(Model model, User user)
 	{
 		userService.create(user);
 		return "main";
 	}
-	
+
 	@RequestMapping(value="edit", method=RequestMethod.GET)
-	public String edit(Model model, @RequestParam("id")int id) 
+	public String edit(Model model, @RequestParam("id")int id)
 	{
 		User user = userService.findOne(id);
 		List<Department> departments = departmentService.findAll();
@@ -185,14 +190,14 @@ public class UserController {
 		model.addAttribute("departments",departments);
 		return "user/edit";
 	}
-	
+
 	@RequestMapping(value="edit", method=RequestMethod.POST)
-	public String edit(Model model, User user) 
+	public String edit(Model model, User user)
 	{
 		userService.update(user);
 		return "main";
 	}
-	
+
 	@RequestMapping("delete")
 	public String delete()
 	{
